ff05dfbfdec73dcdf6848f4beed49911847014db
mxnv <vadim.beller@mail.ru>
2024-09-28 20:56:34 +0300

    #13 add querydsl

--- src/main/java/com/petregisterofequipmentnew/entities/repositories/ProductPredicate.java
+++ src/main/java/com/petregisterofequipmentnew/entities/repositories/ProductPredicate.java
@@ -5,10 +5,7 @@ import com.petregisterofequipmentnew.TypeEquipmentEnum;
 import com.petregisterofequipmentnew.entities.QProduct;
 import com.petregisterofequipmentnew.others.DirectionSort;
 import com.petregisterofequipmentnew.others.ParametersSort;
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.Expressions;
 
 public class ProductPredicate {
 
@@ -17,12 +14,21 @@ public class ProductPredicate {
                                             Integer price, Integer size, Boolean isAvailability, Integer countsDoor,
                                             String typeCompressor, Integer sizeDustCollect, Integer countsRegime,
                                             String typeProcessor,
-                                            String category, Integer memoryPhone, Integer countsSnaps, String technology,
-                                            Integer offset, Integer limit,
-                                            ParametersSort parametersSort, DirectionSort directionSort) {
+                                            String category, Integer memoryPhone, Integer countsSnaps, String technology) {
         QProduct product = QProduct.product;
         BooleanExpression booleanExpression = product.nameProduct.eq(nameProduct);
 
+        booleanExpression = commonCharacteristicsProduct(booleanExpression, product, typeEquipmentEnum, colorEquipment, price, size, isAvailability);
+        if (typeEquipmentEnum != null) {
+            booleanExpression = individualAttributesProducts(booleanExpression, typeEquipmentEnum, product, countsDoor, typeCompressor, sizeDustCollect,
+                    countsRegime, typeProcessor, category, memoryPhone, countsSnaps, technology);
+        }
+        return booleanExpression;
+    }
+
+    private BooleanExpression commonCharacteristicsProduct(BooleanExpression booleanExpression, QProduct product,
+                                                           TypeEquipmentEnum typeEquipmentEnum, ColorEquipment colorEquipment,
+                                                           Integer price, Integer size, Boolean isAvailability) {
         if (typeEquipmentEnum != null) {
             booleanExpression = booleanExpression.and(product.nameTypeTechnic.eq(typeEquipmentEnum));
         }
@@ -38,16 +44,56 @@ public class ProductPredicate {
         if (isAvailability != null) {
             booleanExpression = booleanExpression.and(product.attributes.isAvailabilityProducts.eq(isAvailability));
         }
-        if (typeEquipmentEnum != null) {
-            if (countsDoor != null) {
-                if (typeEquipmentEnum.equals(TypeEquipmentEnum.FRIDGE))
+        return booleanExpression;
+    }
+
+    private BooleanExpression individualAttributesProducts(BooleanExpression booleanExpression, TypeEquipmentEnum typeEquipmentEnum,
+                                                           QProduct product, Integer countsDoor, String typeCompressor, Integer sizeDustCollect,
+                                                           Integer countsRegime, String typeProcessor, String category, Integer memoryPhone,
+                                                           Integer countsSnaps, String technology) {
+        switch (typeEquipmentEnum) {
+            case FRIDGE -> {
+                if (countsDoor != null) {
                     booleanExpression = booleanExpression.and(product.attributes.countsDoor.eq(countsDoor));
-            }
-            if (typeCompressor != null) {
-                if (typeEquipmentEnum.equals(TypeEquipmentEnum.FRIDGE))
+                }
+                if (typeCompressor != null) {
                     booleanExpression = booleanExpression.and(product.attributes.typeCompressor.eq(typeCompressor));
+                }
+            }
+            case HOOVER -> {
+                if (sizeDustCollect != null) {
+                    booleanExpression = booleanExpression.and(product.attributes.sizeDustCollect.eq(sizeDustCollect));
+                }
+                if (countsRegime != null) {
+                    booleanExpression = booleanExpression.and(product.attributes.countsRegime.eq(countsRegime));
+                }
+            }
+            case PERSONAL_COMPUTER -> {
+                if (typeProcessor != null) {
+                    booleanExpression = booleanExpression.and(product.attributes.typeCompressor.eq(typeCompressor));
+                }
+                if (category != null) {
+                    booleanExpression = booleanExpression.and(product.attributes.category.eq(category));
+                }
+            }
+            case SMARTPHONE -> {
+                if (memoryPhone != null) {
+                    booleanExpression = booleanExpression.and(product.attributes.memoryPhone.eq(memoryPhone));
+                }
+                if (countsSnaps != null) {
+                    booleanExpression = booleanExpression.and(product.attributes.countsRegime.eq(countsRegime));
+                }
+            }
+            case TELEVISION -> {
+                if (category != null) {
+                    booleanExpression = booleanExpression.and(product.attributes.category.eq(category));
+                }
+                if (technology != null) {
+                    booleanExpression = booleanExpression.and(product.attributes.technology.eq(technology));
+                }
             }
         }
+        return booleanExpression;
     }
 
 }
