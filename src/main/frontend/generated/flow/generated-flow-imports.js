f1afbb589d36d99fb37e1f9169d050f6ee109195
vsirik <vsirik@niisokb.ru>
2024-11-22 17:10:12 +0300

    fix

---
+++ src/main/frontend/generated/flow/generated-flow-imports.js
@@ -0,0 +1,25 @@
+import '@vaadin/common-frontend/ConnectionIndicator.js';
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/vaadin-lumo-styles/color-global.js';
+import '@vaadin/vaadin-lumo-styles/typography-global.js';
+import '@vaadin/vaadin-lumo-styles/sizing.js';
+import '@vaadin/vaadin-lumo-styles/spacing.js';
+import '@vaadin/vaadin-lumo-styles/style.js';
+import '@vaadin/vaadin-lumo-styles/vaadin-iconset.js';
+
+const loadOnDemand = (key) => {
+  const pending = [];
+  if (key === 'a20486972cddc7047c90a8d32625037a56dfe78192ef4ef25221189e416a0152') {
+    pending.push(import('./chunks/chunk-ed31768d0f494de48d6d5a57f6560300bed1359ffa9db36b3d9dd31539eea1be.js'));
+  }
+  return Promise.all(pending);
+}
+
+window.Vaadin = window.Vaadin || {};
+window.Vaadin.Flow = window.Vaadin.Flow || {};
+window.Vaadin.Flow.loadOnDemand = loadOnDemand;
+window.Vaadin.Flow.resetFocus = () => {
+ let ae=document.activeElement;
+ while(ae&&ae.shadowRoot) ae = ae.shadowRoot.activeElement;
+ return !ae || ae.blur() || ae.focus() || true;
+}
